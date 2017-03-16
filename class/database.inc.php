92fd6bda595bd37dba4a72e9ad183728e65da1a1
Mark Diez <markediez@gmail.com>
2017-03-16 11:48:13 -0700

    Refactor database.inc to use mysqli

--- class/database.inc.php
+++ class/database.inc.php
@@ -4,61 +4,61 @@ class database {
 	var private $initialized = false;
 	var private $handle = null; // database connection handle
 	var private $last_result = null;
-	
+
 	static public function query($sql) {
 		if( ! database::$initialized) database::init();
-		
-		$result = mysql_query($sql);
+
+		$result = mysqli_query(database::$handle, $sql);
 		if($result == false) {
-			echo "An error occurred while executing the statement: ".mysql_error()."<br />";
+			echo "An error occurred while executing the statement: ".mysqli_error()."<br />";
 			return(false);
 		}
-		
+
 		database::$last_result = $result;
-		
+
 		if($result === true) return true; // insert, update, delete, drop does this
-		
+
 		$results = array();
-		while($row = mysql_fetch_assoc($result)) {
+		while($row = mysqli_fetch_assoc($result)) {
 			$results[] = $row;
 		}
-		
+
 		return $results;
 	}
-	
+
 	static private function init() {
-		database::$handle = mysql_connect("localhost", "hc", "admin");
-		
+		database::$handle = mysqli_connect("localhost", "hc", "admin");
+
 		if(database::$handle == false) {
-			echo "Could not connect to database: ".mysql_error()."<br />";
+			echo "Could not connect to database: ".mysqli_error()."<br />";
 			return;
 		}
-		
-		$err = mysql_select_db("calaggie", database::$handle);
+
+		$err = mysqli_select_db(database::$handle, "calaggie");
 		if($err == false) {
-			echo "Connected but could not select database: ".mysql_error()."<br />";
+			echo "Connected but could not select database: ".mysqli_error()."<br />";
 			return;
 		}
-		
+
 		database::$initialized = true;
 	}
-	
+
 	static public function escape($str) {
 		if( ! database::$initialized) database::init();
-		
+
 		if( get_magic_quotes_gpc() )
 			return $str;
 		else
-			return mysql_real_escape_string($str);
+			return mysqli_real_escape_string(database::$handle, $str);
 	}
-	
+
 	static public function num_rows() {
 		if( ! database::$initialized) database::init();
-	
-		return mysql_num_rows(database::$last_result);
+
+		return mysqli_num_rows(database::$last_result);
 	}
-	
+
 	static public function insert_id() {
-		return mysql_insert_id(database::$handle);
+		return mysqli_insert_id(database::$handle);
 	}
 };
