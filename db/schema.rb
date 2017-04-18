a7d9895e8f3d3029091073799d7df1d3aac753f3
Mark Diez <markediez@gmail.com>
2017-04-18 12:08:07 -0700

    Rename tid and sid to topic_id and image_id:

---
+++ db/schema.rb
@@ -0,0 +1,85 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170418190046) do
+
+  create_table "collections", force: :cascade do |t|
+    t.string "name", limit: 255, default: "", null: false
+    t.string "code", limit: 50,  default: "", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string  "file",       limit: 96,  default: "", null: false
+    t.string  "thumbnail",  limit: 128, default: "", null: false
+    t.string  "title",      limit: 255, default: "", null: false
+    t.text    "card",                                null: false
+    t.text    "citation",                            null: false
+    t.integer "collection",             default: 0,  null: false
+    t.integer "public",                 default: 0,  null: false
+    t.integer "views",                  default: 0,  null: false
+    t.integer "featured",               default: 0,  null: false
+    t.text    "notes",                               null: false
+    t.text    "s3"
+    t.index ["title", "card", "citation", "notes"], name: "idx_images_searchable"
+  end
+
+  create_table "keyword_assignments", primary_key: ["sid", "kid"], force: :cascade do |t|
+    t.integer "sid", default: 0, null: false
+    t.integer "kid", default: 0, null: false
+    t.index ["sid", "kid"], name: "sqlite_autoindex_keyword_assignments_1", unique: true
+  end
+
+  create_table "keywords", force: :cascade do |t|
+    t.string "title", limit: 64, default: "", null: false
+    t.index ["title"], name: "sqlite_autoindex_keywords_1", unique: true
+  end
+
+  create_table "region_assignments", force: :cascade do |t|
+    t.integer "sid", default: 0, null: false
+    t.integer "rid", default: 0, null: false
+  end
+
+  create_table "regions", force: :cascade do |t|
+    t.string "title", limit: 64, default: "", null: false
+  end
+
+  create_table "standards_cal", force: :cascade do |t|
+    t.integer "grade_id",               default: 0,  null: false
+    t.string  "standard_id", limit: 50, default: "", null: false
+    t.text    "description",                         null: false
+  end
+
+  create_table "standards_data", force: :cascade do |t|
+    t.integer "image_id", default: 0, null: false
+    t.integer "sid",      default: 0, null: false
+    t.integer "stype",    default: 0, null: false
+  end
+
+  create_table "standards_nat", force: :cascade do |t|
+    t.integer "era",      default: 0, null: false
+    t.integer "us_world", default: 0, null: false
+    t.text    "title",                null: false
+  end
+
+  create_table "topic_assignments", force: :cascade do |t|
+    t.integer "image_id", default: 0, null: false
+    t.integer "topic_id", default: 0, null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string  "code",       limit: 3,  default: "",   null: false
+    t.string  "title",      limit: 50, default: "",   null: false
+    t.text    "collection",            default: "US", null: false
+    t.integer "featured",              default: 0,    null: false
+  end
+
+end
