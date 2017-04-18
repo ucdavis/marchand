13f886be64539d5d63a58a93853b834deb789042
Mark Diez <markediez@gmail.com>
2017-04-18 11:04:11 -0700

    Remove php files

--- php/js/main.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-
-function toggle(d)
-{
-        var o=document.getElementById(d);
-        o.style.display=(o.style.display=='block')?'none':'block';
-}
--- php/snippets/sidequote.php
+++
@@ -1,6 +0,0 @@
-<?php
-	echo '<em>&#8220;' . $quote['quote'] . '&#8221;<br /><br />';
-	echo $quote['name'] . '<br />';
-	if(isset( $quote['position']) && ($quote['position'] != "")){ echo $quote['position'] . '<br />'; } 
-	echo $quote['organization'] . '</em>';
-?>
