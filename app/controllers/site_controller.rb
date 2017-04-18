0c3d770b522f03a9a5586e6e8f0f9c621647b35c
Mark Diez <markediez@gmail.com>
2017-04-18 14:37:05 -0700

    Add bestof functionality

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,7 +3,13 @@ class SiteController < ApplicationController
 
 	def index
 		# Retrieve featured topics and images available to the public
-		@cards = TopicAssignment.joins(:image).joins(:topic).where(:images => {:featured => 1, :public => 1}, :topics => {:featured => 1}).order("RANDOM()").limit(IMAGE_LIMIT)
+		if params[:bestof].present?
+			@search = true
+			@cards = TopicAssignment.joins(:image).joins(:topic).where(:images => {:featured => 1, :public => 1}, :topics => {:featured => 1, }, :topic_id => params[:bestof])
+		else
+			@search = false
+			@cards = TopicAssignment.joins(:image).joins(:topic).where(:images => {:featured => 1, :public => 1}, :topics => {:featured => 1}).order("RANDOM()").limit(IMAGE_LIMIT)
+		end
 
 	end
 
