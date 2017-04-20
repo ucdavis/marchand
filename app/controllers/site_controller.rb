dd049817561216e0ce2018946ff3832282e38f51
Mark Diez <markediez@gmail.com>
2017-04-20 11:47:37 -0700

    Link featured images from index to search page '

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,14 +3,13 @@ class SiteController < ApplicationController
 
 	def index
 		# Retrieve featured topics and images available to the public
-		if params[:bestof].present?
-			@search = true
-			@cards = TopicAssignment.joins(:image).joins(:topic).where(:images => {:featured => 1, :public => 1}, :topics => {:featured => 1, }, :topic_id => params[:bestof])
-		else
-			@search = false
-			@cards = TopicAssignment.joins(:image).joins(:topic).where(:images => {:featured => 1, :public => 1}, :topics => {:featured => 1}).order("RANDOM()").limit(IMAGE_LIMIT)
-		end
+		@cards = TopicAssignment.joins(:image).joins(:topic).where(:images => {:featured => 1, :public => 1}, :topics => {:featured => 1}).order("RANDOM()").limit(IMAGE_LIMIT)
+	end
 
+	def search
+		if params[:bestof].present? && params[:bestof]
+			@cards = TopicAssignment.joins(:image).joins(:topic).where(:images => {:featured => 1, :public => 1}, :topics => {:featured => 1, }, :topic_id => params[:topic_id])
+		end
 	end
 
 	def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'site#index'
-  get '/edit', to: 'site#edit'
+	get '/edit', to: 'site#edit'
+	get '/search', to: 'site#search'
 end
