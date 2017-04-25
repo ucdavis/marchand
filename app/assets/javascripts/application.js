2d834ad1c6bfd026faeb36766a581a3922395d1d
shanglu9425 <shanglu9425ll@hotmail.com>
2017-04-25 09:36:43 -0700

    solved fixed advanced search panel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,36 @@
 //= require turbolinks
 //= require bootstrap.min.js
 //= require_tree .
+
+$(document).ready(function(){
+
+    var StickyElement = function(node){
+  var doc = $(document), 
+      fixed = false,
+      anchor = node.find('.sticky-anchor'),
+      content = node.find('.sticky-content');
+  
+  var onScroll = function(e){
+    var docTop = doc.scrollTop(),
+        anchorTop = anchor.offset().top;
+    
+    console.log('scroll', docTop, anchorTop);
+    if(docTop > anchorTop){
+      if(!fixed){;
+        content.addClass('fixed');        
+        fixed = true;
+      }
+    }  else   {
+      if(fixed){
+        anchor.height(0);
+        content.removeClass('fixed'); 
+        fixed = false;
+      }
+    }
+  };
+  
+  $(window).on('scroll', onScroll);
+};
+
+var demo = new StickyElement($('#sticky'));
+});
