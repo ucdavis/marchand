cdc8b9d1b8c6b28be897fae420cbbace4b028563
Mark Diez <markediez@gmail.com>
2017-04-25 16:13:25 -0700

    Fix some style

--- app/models/cal_standard.rb
+++ app/models/cal_standard.rb
@@ -1,5 +1,6 @@
 class CalStandard < ActiveRecord::Base
     has_many :data_cal_standards
+    
   def label
     grade_id = (self.grade_id == 0) ? "K" : self.grade_id
 
--- app/models/nat_standard.rb
+++ app/models/nat_standard.rb
@@ -2,5 +2,6 @@ class NatStandard < ActiveRecord::Base
     validates_presence_of :era
     validates_presence_of :us_world
     validates_presence_of :title
+
     has_many :data_nat_standards
 end
