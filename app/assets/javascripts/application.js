dcfcb346fe73d340733b4bfa08ddebccd99ca81f
Mark Diez <markediez@gmail.com>
2017-04-25 11:40:13 -0700

    Move modal to search page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,32 +19,32 @@
 $(document).ready(function(){
 
     var StickyElement = function(node){
-  var doc = $(document), 
-      fixed = false,
-      anchor = node.find('.sticky-anchor'),
-      content = node.find('.sticky-content');
-  
-  var onScroll = function(e){
-    var docTop = doc.scrollTop(),
-        anchorTop = anchor.offset().top;
-    
-    console.log('scroll', docTop, anchorTop);
-    if(docTop > anchorTop){
-      if(!fixed){;
-        content.addClass('fixed');        
-        fixed = true;
-      }
-    }  else   {
-      if(fixed){
-        anchor.height(0);
-        content.removeClass('fixed'); 
-        fixed = false;
-      }
-    }
-  };
-  
-  $(window).on('scroll', onScroll);
-};
+        var doc = $(document),
+        fixed = false,
+        anchor = node.find('.sticky-anchor'),
+        content = node.find('.sticky-content');
 
-var demo = new StickyElement($('#sticky'));
+        var onScroll = function(e){
+            var docTop = doc.scrollTop(),
+            anchorTop = anchor.offset().top;
+
+            console.log('scroll', docTop, anchorTop);
+            if(docTop > anchorTop){
+                if(!fixed){;
+                    content.addClass('fixed');
+                    fixed = true;
+                }
+            }  else   {
+                if(fixed){
+                    anchor.height(0);
+                    content.removeClass('fixed');
+                    fixed = false;
+                }
+            }
+        };
+
+        $(window).on('scroll', onScroll);
+    };
+
+    var demo = new StickyElement($('#sticky'));
 });
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,17 +1,17 @@
 class SiteController < ApplicationController
-	IMAGE_LIMIT = 24
+    IMAGE_LIMIT = 24
 
-	def index
-		# Retrieve featured topics and images available to the public
-		@cards = TopicAssignment.joins(:image).joins(:topic).where(:images => {:featured => 1, :public => 1}, :topics => {:featured => 1}).order("RANDOM()").limit(IMAGE_LIMIT)
-	end
+    def index
+        # Retrieve featured topics and images available to the public
+        @cards = TopicAssignment.joins(:image).joins(:topic).where(:images => {:featured => 1, :public => 1}, :topics => {:featured => 1}).order("RANDOM()").limit(IMAGE_LIMIT)
+    end
 
-	def search
-		if params[:bestof].present? && params[:bestof]
-			@cards = TopicAssignment.joins(:image).joins(:topic).where(:images => {:featured => 1, :public => 1}, :topics => {:featured => 1, }, :topic_id => params[:topic_id])
-		end
-	end
+    def search
+        if params[:bestof].present? && params[:bestof]
+            @cards = TopicAssignment.joins(:image).joins(:topic).where(:images => {:featured => 1, :public => 1}, :topics => {:featured => 1, }, :topic_id => params[:topic_id])
+        end
+    end
 
-	def edit
-	end
+    def edit
+    end
 end
