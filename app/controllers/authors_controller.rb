fa09164a8f15d942e26e04599765632fb74c20c1
Mark Diez <markediez@gmail.com>
2017-06-14 17:07:13 -0700

    Add authors to admin control panel

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,16 @@
+class AuthorsController < ApplicationController
+    def new
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def create
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
 
     resources :images
     resources :topics
+    resources :authors
 end
