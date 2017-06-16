0a3cb9274bc9539349b9ec298f0ec46d63e73aca
Mark Diez <markediez@gmail.com>
2017-06-16 12:48:46 -0700

    Add ES attributes to lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,11 @@
 class Lesson < ApplicationRecord
+    include Elasticsearch::Model
+    include Elasticsearch::Model::Callbacks
+
+    after_update -> {
+      __elasticsearch__.index_document
+    }
+
     has_many :lesson_authors, :dependent => :destroy
     has_many :authors, through: :lesson_authors
 
@@ -6,4 +13,24 @@ class Lesson < ApplicationRecord
     has_many :images, through: :lesson_images
 
     has_many :attachments, :dependent => :destroy
+
+    # @param query - text to search for
+    # @param filter - array of hashes in the form of ElasticSearch's filter parameter for queryDSL
+    def self.search(query, filter)
+        q = {
+            bool: {
+                must: query
+            }
+        }
+
+        q[:bool][:filter] = filter.split(",") unless filter.empty?
+        __elasticsearch__.search({ query: q })
+    end
+
+    def as_indexed_json(options={})
+        self.as_json(
+            include: { lesson_authors: { only: :author_id},
+            lesson_images: { only: :image_id }
+        })
+    end
 end
--- lib/tasks/elasticsearch.rake
+++ lib/tasks/elasticsearch.rake
@@ -4,6 +4,7 @@ namespace :es do
 	desc "Reindex all models"
 	task :reindex_all => :environment do
 		Rake::Task["es:reindex_images"].invoke
+        Rake::Task["es:reindex_lessons"].invoke
 	end
 
 	desc "Reindex Image model"
@@ -12,6 +13,15 @@ namespace :es do
 		Image.__elasticsearch__.create_index! :force => true
 		puts "Importing image documents..."
 		Image.__elasticsearch__.import
+		puts "Done."
+	end
+
+    desc "Reindex Lesson model"
+	task :reindex_lessons => :environment do
+		puts "Reindexing images..."
+		Lesson.__elasticsearch__.create_index! :force => true
+		puts "Importing lesson documents..."
+		Lesson.__elasticsearch__.import
 		puts "Done."
 	end
 end
