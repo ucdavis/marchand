1fa89329b407d60bff574f99564e17d9c8284aac
Mark Diez <markediez@gmail.com>
2017-06-16 15:53:44 -0700

    Add code to CRUD a lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -12,21 +12,49 @@ class LessonsController < ApplicationController
     end
 
     def create
+        respond_to do |format|
+            @lesson = Lesson.new(lesson_params)
+            if @lesson.save
+                format.html { redirect_to lessons_path, notice: "Succesfully created lesson" }
+                format.json { head :no_content }
+            else
+                format.html { render action: :edit, error: "Failed to create lesson" }
+                format.json { render json: @lesson.errors, status: :unprocessable_entity }
+            end
+        end
     end
 
     def edit
     end
 
     def update
+        respond_to do |format|
+            if @lesson.update(lesson_params)
+                format.html { redirect_to lessons_path, notice: "Succesfully updated lesson" }
+                format.json { head :no_content }
+            else
+                format.html { render action: :edit, error: "Failed to update lesson" }
+                format.json { render json: @lesson.errors, status: :unprocessable_entity }
+            end
+        end
     end
 
     # DELETE /images/:id
     def destroy
+        respond_to do |format|
+            if @lesson.destroy
+                format.html { redirect_to lessons_path, notice: "Succesfully deleted lesson" }
+                format.json { head :no_content }
+            else
+                format.html { redirect_to lessons_path, error: "Failed to delete lesson" }
+                format.json { render json: @lesson.errors, status: :unprocessable_entity }
+            end
+        end
     end
 
     private
     def lesson_params
-        params.require(:lesson)
+        params.require(:lesson).permit(:id, :grade, :title, :background, :pdf, :author_ids => [], :image_ids => [], :attachment_ids => [])
     end
 
     def set_lesson
