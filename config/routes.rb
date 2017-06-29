5f7dace522fb8063cf23f028bf8f0ddcd0c53778
Christopher Thielen <cmthielen@ucdavis.edu>
2017-06-28 17:47:11 -0700

    Route cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root 'site#index'
-    get '/admin', to: 'site#admin'
-    post '/admin', to: 'site#admin'
-	get '/search', to: 'site#search'
-    get '/download/:key', to: 'site#download'
+  root 'site#index'
 
-    get '/login', to: 'site#login'
-    get '/logout', to: 'site#logout'
+  get '/admin', to: 'site#admin'
+  post '/admin', to: 'site#admin'
+  get '/search', to: 'site#search'
+  get '/download/:key', to: 'site#download'
 
-    post '/authors/:id/edit', to: 'authors#edit'
-    post '/topics/:id/edit', to: 'topics#edit'
-    post '/regions/:id/edit', to: 'regions#edit'
-    post '/cal_standards/:id/edit', to: 'cal_standards#edit'
-    post '/nat_standards/:id/edit', to: 'nat_standards#edit'
+  get '/login', to: 'site#login'
+  get '/logout', to: 'site#logout'
 
-    resources :images
-    resources :authors
-    resources :topics
-    resources :regions
-    resources :cal_standards
-    resources :nat_standards
-    resources :lessons
+  post '/authors/:id/edit', to: 'authors#edit'
+  post '/topics/:id/edit', to: 'topics#edit'
+  post '/regions/:id/edit', to: 'regions#edit'
+  post '/cal_standards/:id/edit', to: 'cal_standards#edit'
+  post '/nat_standards/:id/edit', to: 'nat_standards#edit'
 
+  resources :images
+  resources :authors
+  resources :topics
+  resources :regions
+  resources :cal_standards
+  resources :nat_standards
+  resources :lessons
 end
