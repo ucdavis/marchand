63e9024c0c5b45a9ac09a1ef4af72df969bf6312
Christopher Thielen <cmthielen@ucdavis.edu>
2017-07-05 11:22:13 -0700

    Move image-related code from Site JS to Images JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.min.js
@@ -20,7 +8,8 @@
 
 $(document).ready(function(){
 
-    var StickyElement = function(node){
+    // Provides 'affix' support for search sidebar
+    var StickyElement = function(node) {
         var doc = $(document),
         fixed = false,
         anchor = node.find('.sticky-anchor'),
@@ -52,7 +41,6 @@ $(document).ready(function(){
     };
 
     var stickydemo = new StickyElement($('#sticky'));
-
 
     $("#slider").slider({
         min: 0,
