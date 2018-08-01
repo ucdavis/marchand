4cbfd99d9200c778776737838a51b646c611ffda
Christopher Thielen <cmthielen@ucdavis.edu>
2018-08-01 08:39:29 -0700

    Handle UnknownFormat exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include ApplicationHelper
+  rescue_from ActionController::UnknownFormat, with: :bad_request
+
+  protected
+
+  def bad_request
+    render plain: 'Bad request', status: 400
+  end
 end
