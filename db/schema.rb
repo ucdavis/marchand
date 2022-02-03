06f05a3a589fbbccb7a78d490010b7b3725c22ae
Jarold Wong <cwjwong@ucdavis.edu>
2022-02-03 08:44:46 -0800

    add public and description to featured collections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_212404) do
+ActiveRecord::Schema.define(version: 2020_09_04_160829) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2020_08_03_212404) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "public"
+    t.text "description"
   end
 
   create_table "featured_collections_images", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
